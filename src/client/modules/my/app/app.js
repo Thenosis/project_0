9a7a418ece73574f16c18cb9de2e40aa50c47b8b
Martin Luna II <Martin.lunaii@revature.net>
2022-02-04 16:53:10 -0500

    Fixed ConRen on Home

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -4,26 +4,33 @@ export default class App extends LightningElement {
     isAbout = false;
     isProduct = false;
     isContact = false;
-    isMain = true;
+    isHome = true;
+
+    goToHome() {
+        this.isAbout = false;
+        this.isProduct = false;
+        this.isContact = false;
+        this.isHome = true;
+   }
 
    goToAbout() {
         this.isAbout = true;
         this.isProduct = false;
         this.isContact = false;
-        this.isMain = false;
+        this.isHome = false;
    }
 
    goToProducts() {
         this.isAbout = false;
         this.isProduct = true;
         this.isContact = false;
-        this.isMain = false;
+        this.isHome = false;
    }
 
    goToContact() {
     this.isAbout = false;
     this.isProduct = false;
     this.isContact = true;
-    this.isMain = false;
+    this.isHome = false;
    }
 }
