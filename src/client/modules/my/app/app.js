2e8bdf18777c304839247656d63bdfc0d58a7499
Martin Luna II <Martin.lunaii@revature.net>
2022-02-04 16:02:55 -0500

    New code added to conditionally render

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,6 +1,29 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
+    isAbout = false;
+    isProduct = false;
+    isContact = false;
+    isMain = true;
 
+   goToAbout() {
+        this.isAbout = true;
+        this.isProduct = false;
+        this.isContact = false;
+        this.isMain = false;
+   }
 
+   goToProducts() {
+        this.isAbout = false;
+        this.isProduct = true;
+        this.isContact = false;
+        this.isMain = false;
+   }
+
+   goToContact() {
+    this.isAbout = false;
+    this.isProduct = false;
+    this.isContact = true;
+    this.isMain = false;
+   }
 }
