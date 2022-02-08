23b3d2d6ab8b7cd57c506687303691cd693a77e3
Martin Luna II <Martin.lunaii@revature.net>
2022-02-08 18:11:28 -0500

    added button to form submit that doesn't break page

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,34 +1,23 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-    isAbout = false;
     isProduct = false;
     isContact = false;
     isHome = true;
 
     goToHome() {
-        this.isAbout = false;
         this.isProduct = false;
         this.isContact = false;
         this.isHome = true;
    }
 
-   goToAbout() {
-        this.isAbout = true;
-        this.isProduct = false;
-        this.isContact = false;
-        this.isHome = false;
-   }
-
    goToProducts() {
-        this.isAbout = false;
         this.isProduct = true;
         this.isContact = false;
         this.isHome = false;
    }
 
    goToContact() {
-    this.isAbout = false;
     this.isProduct = false;
     this.isContact = true;
     this.isHome = false;
--- src/client/modules/my/products/products.js
+++ src/client/modules/my/products/products.js
@@ -1,6 +1,13 @@
 import { LightningElement } from 'lwc';
 
 export default class Products extends LightningElement {
+    isSubmit=false;
 
+    goToSubmit() {
+        this.isProduct = false;
+        this.isContact = false;
+        this.isHome = false;
+        this.isSubmit = true;
+   }
 
 }
